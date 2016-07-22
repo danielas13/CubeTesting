320e6040e6bde6ff489075b9a8e4d402736f0bb5
Halldór Snær Kristjánsson <dorisnaer@gmail.com>
2016-07-22 18:38:31 +0000

    UpdatedAgain

---
+++ CubeTesting/Assets/orbit.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class orbit : MonoBehaviour {
+    float rotationSpeed = 5.0f;
+    public Transform _rotateAround;
+
+    public bool orbitSystem = true;
+    void Update()
+    {
+        if(orbitSystem)
+        transform.RotateAround(_rotateAround.position, Vector3.up, 20 * Time.deltaTime);
+    }
+
+}
